96ca35dd6abe52d3fb9196df833477487dfadd19
Michał Zarach <michal.m.zarach@gmail.com>
2018-10-16 10:19:23 +0200

    Implement logout button logic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,22 @@
 <template>
-    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="demko" dark app fixed>
-    
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="demko" dark app fixed class="app-header">
+
         <v-toolbar-title class="ml-0">
             <v-toolbar-side-icon @click.stop="toggle"></v-toolbar-side-icon>
             <span class="font-weight-bold title">DemCouch</span>
         </v-toolbar-title>
-    
+
         <v-spacer></v-spacer>
-    
+
         <lang-switcher/>
-    
+
         <v-btn icon>
             <v-icon>notifications</v-icon>
         </v-btn>
-        <v-btn icon>
+        <v-btn icon @click="logout">
             <v-icon>exit_to_app</v-icon>
         </v-btn>
-    
+
     </v-toolbar>
 </template>
 
@@ -38,6 +38,10 @@
             toggle() {
                 this.drawer = !this.drawer;
                 this.$emit('input', this.drawer);
+            },
+            logout(){
+              this.$store.dispatch('auth/logout')
+                .then(()=> this.$router.push('login'));
             }
         },
         watch: {
@@ -51,6 +55,10 @@
 
 
 <style lang="scss" scoped>
+    .app-header{
+      z-index: 100;
+    }
+
     .d-header {
         width: 100%;
         height: 60px;
