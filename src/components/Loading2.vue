7037581901e1fe6a838fa86cb260b587b160f6cf
Michał Zarach <michal.m.zarach@gmail.com>
2018-11-13 14:39:14 +0100

    New global component loader

---
+++ src/components/Loading2.vue
@@ -0,0 +1,158 @@
+<template>
+  <transition :name="animation">
+    <div
+      class="loading-overlay is-active"
+      :class="{ 'is-full-page': isFullPage }"
+      v-if="isActive">
+      <div class="loading-background" @click="cancel"/>
+      <div class="loading-icon"/>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'v-loading',
+    props: {
+      active: {
+        type: Boolean,
+        required: true
+      },
+      isFullPage: {
+        type: Boolean,
+        default: false
+      },
+      animation: {
+        type: String,
+        default: 'fade'
+      },
+      canCancel: {
+        type: Boolean,
+        default: false
+      },
+      onCancel: {
+        type: Function,
+        default: () => {}
+      }
+    },
+    data() {
+      return {
+        isActive: this.active || false
+      }
+    },
+    watch: {
+      active(value) {
+        this.isActive = value
+      }
+    },
+    methods: {
+      /**
+       * Close the Modal if canCancel.
+       */
+      cancel() {
+        if (!this.canCancel || !this.isActive) return
+        this.close()
+      },
+      /**
+       * Emit events, and destroy modal if it's programmatic.
+       */
+      close() {
+        this.onCancel.apply(null, arguments)
+        this.$emit('close')
+        this.$emit('update:active', false)
+      },
+      /**
+       * Keypress event that is bound to the document.
+       */
+      keyPress(event) {
+        // Esc key
+        if (event.keyCode === 27) this.cancel()
+      }
+    },
+    created() {
+      if (typeof window !== 'undefined') {
+        document.addEventListener('keyup', this.keyPress)
+      }
+    },
+    beforeDestroy() {
+      if (typeof window !== 'undefined') {
+        document.removeEventListener('keyup', this.keyPress)
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @mixin overlay($offset: 0){
+    bottom: $offset;
+    left: $offset;
+    position: absolute;
+    right: $offset;
+    top: $offset;
+  }
+
+  @keyframes spinAround{
+    from{
+      transform: rotate(0deg);
+    }
+    to{
+      transform: rotate(359deg);
+    }
+  }
+
+
+  @mixin loader{
+    animation: spinAround 500ms infinite linear;
+    border: 2px solid $brand-primary;
+    border-radius: 999999px;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    content: "";
+    display: block;
+    height: 1em;
+    position: relative;
+    width: 1em;
+  }
+
+  .loading-overlay {
+    @include overlay;
+    align-items: center;
+    display: none;
+    justify-content: center;
+    overflow: hidden;
+    z-index: 999;
+  &.is-active {
+     display: flex
+   }
+  &.is-full-page {
+     position: fixed;
+  .loading-icon {
+  &:after {
+     top: calc(50% - 2.5em);
+     left: calc(50% - 2.5em);
+     width: 5em;
+     height: 5em;
+   }
+  }
+  }
+  .loading-background {
+    @include overlay;
+    pointer-events: none;
+    background:#7f7f7f;
+    background:rgba(255,255,255,0.4);
+  }
+  .loading-icon {
+    position: relative;
+  &:after {
+     @include loader;
+     position: absolute;
+     top: calc(50% - 1.5em);
+     left: calc(50% - 1.5em);
+     width: 3em;
+     height: 3em;
+     border-width: 0.25em;
+   }
+  }
+  }
+</style>
