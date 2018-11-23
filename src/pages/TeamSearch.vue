10194623819b2ef7e5eb0beb98e0e41d3bd3aa2e
Michał Zarach <michal.m.zarach@gmail.com>
2018-11-23 17:52:15 +0100

    Added pagination

--- src/pages/TeamSearch.vue
+++ src/pages/TeamSearch.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="team-list">
 
+        <v-loading :active="isLoadingFull" :is-full-page="true"/>
 
-
-        <v-container grid-list-xl fluid>
+        <v-container grid-list-xl fluid class="pt-0 pb-0">
             <v-layout wrap>
                 <v-flex xs12 sm4>
                     <v-text-field color="demko" label="Nazwa drużyny" v-model="terms.name" prepend-icon="search"></v-text-field>
@@ -13,13 +13,17 @@
                 </v-flex>
                 <v-flex>
                     <v-slider v-model="terms.radius"
-                     single-line
-          type="number"
-          color="demko"
-          :step="10"
-          :min="0"
-          :max="100"
-          thumb-label="always"
+                              single-line
+                              type="number"
+                              color="demko"
+                              :step="10"
+                              :min="0"
+                              :max="1000"
+                              thumb-label="always"
+                              :disabled="isLoading"
+                              label="Odległość [KM]"
+                              hint="Promień "
+                              persistent-hint
                     ></v-slider>
                 </v-flex>
             </v-layout>
@@ -27,10 +31,14 @@
 
         <v-container grid-list-xs text-xs-center>
             <v-layout row wrap fill-height>
-                <team v-for="team in teams" :key="team.id" :team="team" />
+              <v-loading :is-full-page="false" :active="isLoading" />
+                <team v-for="team in teams" :key="team.id" :team="team" :allowJoin="true" @join="joinToTeam"/>
             </v-layout>
         </v-container>
 
+      <div class="text-xs-center mb-3" v-if="!isLoading">
+        <v-pagination v-model="currentPage" color="demko" :length="pageCount" circle></v-pagination>
+      </div>
 
     </div>
 </template>
@@ -45,6 +53,7 @@
         },
         data() {
             return {
+                isLoading: false,
                 terms:{
                     name: '',
                     lat: null,
@@ -52,6 +61,8 @@
                     radius: null,
                     order_by: null,
                 },
+                currentPage: 1,
+                pageCount: 1,
                teams: []
             }
         },
@@ -69,24 +80,37 @@
                     this.fetchTeams();
                 },
                 deep: true
+            },
+            currentPage(){
+              this.fetchTeams();
             }
         },
         methods: {
            fetchTeams(){
-               //this.$eventBus.$emit('loading', true);
-               this.$http.get('/teams', {params: this.params}).then(({data}) => {
+               this.isLoadingFull = false;
+               this.isLoading = true;
+               this.$http.get('/teams', {params: {...this.params, page: this.currentPage, per_page: 8}}).then(({data}) => {
                     this.teams = data.data;
                     this.pageCount = data.pagination.page_count;
-                    //this.$eventBus.$emit('loading', false);
-                }).finally(() => this.$eventBus.$emit('loading', false))
+                }).finally(() => this.isLoading = false)
            },
            getAddressData(addressData, placeResultData) {
-                this.terms.lat = addressData ? addressData.latitude : null,
-                this.terms.lng = addressData ? addressData.longitude : null
+                this.terms.lat = addressData ? addressData.latitude : null;
+                this.terms.lng = addressData ? addressData.longitude : null;
             },
+            joinToTeam(id){
+              this.isLoadingFull = true;
+              this.$http.post('/team-memberships', {team_id: id}).then(() => {
+                this.$eventBus.$emit('info', 'Wysłano prośbę o dołączenie')
+              }).finally(() => this.isLoadingFull = false);
+            }
         },
         created() {
             this.fetchTeams();
+        },
+        destroyed() {
+          // https://github.com/MadimetjaShika/vuetify-google-autocomplete/issues/56
+          window.vgaMapState.initMap = true
         },
     }
 </script>
